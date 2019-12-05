5d82fec72998ef217399afe08483826e066a7adc
geneonosis <genetigner.art@gmail.com>
2019-12-05 02:43:36 -0600

    Pause menu done
    pause menu features are exit the game, quit to main menu, restart race, continue

---
+++ Assets/KeyboardInGameCommands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInGameCommands : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}

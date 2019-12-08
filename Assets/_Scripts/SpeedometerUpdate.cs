0e9b8a9191433cc5212d9f7002de4bc6b75bd18e
scottlandunderwood <scottland.underwood@gmail.com>
2019-12-07 22:33:34 -0600

    speedometer added to Tokyo HUD

---
+++ Assets/_Scripts/SpeedometerUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SpeedometerUpdate : MonoBehaviour
+{
+    public GameObject vehicle;
+    public DriveScript driveScript;
+    public TextMeshProUGUI text;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        driveScript = vehicle.GetComponent<DriveScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        text.SetText(driveScript.currentSpeed.ToString() + " MPH");
+    }
+}
